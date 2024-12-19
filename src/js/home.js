e46116680532042aaaefc7c9ce4a31a7f816d3dc
Yohann <Lai>
2024-12-19 01:09:16 +0100

    Update CSS and add animation to home page

---
+++ src/js/home.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const helloText = document.querySelector("#hello");
+    const homeMenu = document.querySelector("#home-menu");
+
+    setTimeout(() => {
+        helloText.style.opacity = "1";
+        helloText.style.transform = "translateY(0)";
+
+        setTimeout(() => {
+            homeMenu.style.opacity = "1";
+            homeMenu.style.transform = "scale(1)";
+        }, 1000)
+    }, 500);
+});

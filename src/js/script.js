76b4deed284f929b04a0ab3471531c3cc91f44bb
Lilian Peuron <lilianpeuron@gmail.com>
2024-12-22 14:57:24 +0100

    Add data access verification

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,8 @@ const info = document.getElementById("info");
 
 const hourly_forecast = document.getElementsByClassName("hourly-time");
 const hourly_temp = document.getElementsByClassName("hourly-temperature");
-const hourly_icon_mobile = document.getElementsByClassName("hourly-icon_mobile");
+const hourly_icon_mobile =
+  document.getElementsByClassName("hourly-icon_mobile");
 const hourly_icon = document.getElementsByClassName("hourly-icon");
 
 const hourly_forecast_mobile =
@@ -20,7 +21,7 @@ const hourly_temp_mobile = document.getElementsByClassName(
   "hourly-temperature-mobile"
 );
 
-const forecast_time = document.getElementsByClassName("forecast-time");
+const forecast_time = document.querySelectorAll(".forecast-time");
 const forecast_temp = document.getElementsByClassName("forecast-temperature");
 const forecast_icon = document.getElementsByClassName("forecast-icon");
 
@@ -28,95 +29,126 @@ const weatherBGColor = document.getElementById("weatherBGColor");
 
 const forecastItems = document.getElementsByClassName("daily-forecast-item");
 
-
 if (window.innerHeight < 768 || window.innerWidth < 768) {
-    for (let i = 4; i < forecastItems.length; i++) {
-        forecastItems[i].style.display = "none";
-    }
+  for (let i = 4; i < forecastItems.length; i++) {
+    forecastItems[i].style.display = "none";
+  }
 }
 
 async function loadWeather(city) {
-    const response_current = await fetch(API_URL_CURRENT + "&q=" + city);
-    const data_current = await response_current.json();
-    
-    console.log(data_current);
-    
-    current_temp.innerHTML = data_current.current.temp_c + "°C";
-    
-    if (data_current.current.temp_c < 0) {
-        info.innerHTML = "Olala, glagla !";
-    } else if (data_current.current.temp_c >= 0 && data_current.current.temp_c < 10) {
-        info.innerHTML = "Couvrez-vous !";
-    } else if (data_current.current.temp_c >= 10 && data_current.current.temp_c < 20) {
-        info.innerHTML = "Il fait bon !";
-    } else if (data_current.current.temp_c >= 20 && data_current.current.temp_c < 30) {
-        info.innerHTML = "C'est l'été ouuuuuu ???";
-    } else if (data_current.current.temp_c >= 30) {
-        info.innerHTML = "Petite binouze bien fraiche ?";
+  const response_current = await fetch(API_URL_CURRENT + "&q=" + city);
+  const data_current = await response_current.json();
+
+  console.log(data_current);
+
+  current_temp.innerHTML = data_current.current?.temp_c + "°C";
+
+  if (data_current.current?.temp_c < 0) {
+    info.innerHTML = "Olala, glagla !";
+  } else if (
+    data_current.current?.temp_c >= 0 &&
+    data_current.current?.temp_c < 10
+  ) {
+    info.innerHTML = "Couvrez-vous !";
+  } else if (
+    data_current.current?.temp_c >= 10 &&
+    data_current.current?.temp_c < 20
+  ) {
+    info.innerHTML = "Il fait bon !";
+  } else if (
+    data_current.current?.temp_c >= 20 &&
+    data_current.current?.temp_c < 30
+  ) {
+    info.innerHTML = "C'est l'été ouuuuuu ???";
+  } else if (data_current.current?.temp_c >= 30) {
+    info.innerHTML = "Petite binouze bien fraiche ?";
+  }
+
+  for (var i = 0, max = current_condition.length; i < max; i++) {
+    current_condition[i].innerHTML = data_current.current?.condition?.text;
+  }
+
+  const date = new Date(data_current.location?.localtime_epoch * 1000);
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const formattedTime = `${hour}:${minute < 10 ? "0" : ""}${minute}`;
+
+  if (window.innerWidth < 768) {
+    for (let i = 0; i < 4; i++) {
+      const forecastHour = (hour + i + 1) % 24;
+      const response_forecast = await fetch(
+        API_URL_FORECAST + "&q=" + city + "&hour=" + forecastHour
+      );
+      const data_forecast = await response_forecast.json();
+      hourly_forecast_mobile[i].innerHTML = forecastHour + ":00";
+      hourly_temp_mobile[i].innerHTML =
+        data_forecast.forecast?.forecastday[0]?.day?.avgtemp_c + "°C";
+      hourly_icon_mobile[i].src =
+        data_forecast.forecast?.forecastday[0]?.day?.condition?.icon;
     }
-
-    for (var i = 0, max = current_condition.length; i < max; i++) {
-        current_condition[i].innerHTML = data_current.current.condition.text;
+  } else {
+    for (let i = 0; i < 8; i++) {
+      const forecastHour = (hour + i + 1) % 24;
+      const response_forecast = await fetch(
+        API_URL_FORECAST + "&q=" + city + "&hour=" + forecastHour
+      );
+      const data_forecast = await response_forecast.json();
+      hourly_forecast[i].innerHTML = forecastHour + ":00";
+      hourly_temp[i].innerHTML =
+        data_forecast.forecast?.forecastday[0]?.day?.avgtemp_c + "°C";
+      hourly_icon[i].src =
+        data_forecast.forecast?.forecastday[0]?.day?.condition?.icon;
     }
+  }
 
-    const date = new Date(data_current.location.localtime_epoch * 1000);
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    const formattedTime = `${hour}:${minute < 10 ? "0" : ""}${minute}`;
+  const response_forecast = await fetch(
+    API_URL_FORECAST + "&q=" + city + "&days=7"
+  );
+  const data_forecast = await response_forecast.json();
 
-    if (window.innerWidth < 768) {
-        for (let i = 0; i < 4; i++) {
-            const forecastHour = (hour + i + 1) % 24;
-            const response_forecast = await fetch(API_URL_FORECAST + "&q=" + city + "&hour=" + forecastHour);
-            const data_forecast = await response_forecast.json();
-            hourly_forecast_mobile[i].innerHTML = forecastHour + ":00";
-            hourly_temp_mobile[i].innerHTML = data_forecast.forecast.forecastday[0].day.avgtemp_c + "°C";
-            hourly_icon_mobile[i].src = data_forecast.forecast.forecastday[0].day.condition.icon;
-        }
-    } else {
-        for (let i = 0; i < 8; i++) {
-            const forecastHour = (hour + i + 1) % 24;
-            const response_forecast = await fetch(API_URL_FORECAST + "&q=" + city + "&hour=" + forecastHour);
-            const data_forecast = await response_forecast.json();
-            hourly_forecast[i].innerHTML = forecastHour + ":00";
-            hourly_temp[i].innerHTML = data_forecast.forecast.forecastday[0].day.avgtemp_c + "°C";
-            hourly_icon[i].src = data_forecast.forecast.forecastday[0].day.condition.icon;
-        }
-    }
+  for (let i = 0; i < 7; i++) {
+    forecast_time[i].innerHTML =
+      data_forecast.forecast?.forecastday[i]?.date_epoch;
+    const date = new Date(
+      data_forecast.forecast?.forecastday[i]?.date_epoch * 1000
+    );
+    const options = { weekday: "long" };
+    forecast_time[i].innerHTML =
+      date.toLocaleDateString(undefined, options) +
+      " " +
+      date.getDate() +
+      "/" +
+      date.getMonth();
+    forecast_temp[i].innerHTML =
+      data_forecast.forecast?.forecastday[i]?.day?.mintemp_c +
+      "°C, " +
+      data_forecast.forecast?.forecastday[i]?.day?.maxtemp_c +
+      "°C";
+    forecast_icon[i].src =
+      data_forecast.forecast?.forecastday[i]?.day?.condition?.icon;
+  }
 
-    const response_forecast = await fetch(API_URL_FORECAST + "&q=" + city + "&days=7");
-    const data_forecast = await response_forecast.json();
+  const conditionCode = data_current.current?.condition?.code;
 
-    for (let i = 0; i < 7; i++) {
-        forecast_time[i].innerHTML = data_forecast.forecast.forecastday[i].date_epoch;
-        const date = new Date(data_forecast.forecast.forecastday[i].date_epoch * 1000);
-        const options = { weekday: "long" };
-        forecast_time[i].innerHTML = date.toLocaleDateString(undefined, options) + " " + date.getDate() + "/" + date.getMonth(); 
-        forecast_temp[i].innerHTML = data_forecast.forecast.forecastday[i].day.mintemp_c + "°C, " + data_forecast.forecast.forecastday[i].day.maxtemp_c + "°C";
-        forecast_icon[i].src = data_forecast.forecast.forecastday[i].day.condition.icon;
-    }
+  if (conditionCode == 1000) {
+    weatherBGColor.style.backgroundColor = "#FFE142";
+  }
 
-    const conditionCode = data_current.current.condition.code;
+  if (conditionCode >= 1003 && conditionCode <= 1030) {
+    weatherBGColor.style.backgroundColor = "#42C6FF";
+  }
 
-    if (conditionCode == 1000) {
-        weatherBGColor.style.backgroundColor = "#FFE142";
-    }
-
-    if (conditionCode >= 1003 && conditionCode <= 1030) {
-        weatherBGColor.style.backgroundColor = "#42C6FF";
-    }
-
-    if (conditionCode >= 1063 && conditionCode <= 1282) {
-        weatherBGColor.style.backgroundColor = "#FF64D4";
-    }
+  if (conditionCode >= 1063 && conditionCode <= 1282) {
+    weatherBGColor.style.backgroundColor = "#FF64D4";
+  }
 }
 
 document.getElementById("city").addEventListener("change", function () {
-    const city = document.getElementById("city").value;
-    loadWeather(city);
+  const city = document.getElementById("city").value;
+  loadWeather(city);
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    const city = document.getElementById("city").value;
-    loadWeather(city);
+  const city = document.getElementById("city").value;
+  loadWeather(city);
 });
